1acc5f59daf8c4703ccbba5f56b4d6bb3893355f
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-22 19:42:05 +0200

    Add basic route structure, home and navbar components and ensure dev url is home

---
+++ src/components/Homepage.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+function Homepage() {
+    return (
+        <div>Homepage!</div>
+    )
+}
+
+export { Homepage }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export { Navbar }
