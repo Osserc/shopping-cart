4f1251f32701f8cd7722513766b854ca85eb9be9
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-28 21:18:27 +0200

    Add products seed *partially)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Homepage } from './components/Homepage'
 import { About } from './components/About'
 import { Shop } from './components/Shop'
 import { Cart } from './components/Cart'
+import { Products } from './ProductLists'
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  function addItem(event) {
+    let newElement = event.target.index
+    setCart(cart => [...cart, newElement])
+  }
+
+  function removeItem(event) {
+    let item = event.target.index
+    setCart(cart => cart.filter((product) => product !== item))
+  }
+
   return (
     <BrowserRouter>
     <Navbar />
