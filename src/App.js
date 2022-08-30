53f39135ea63a6c5a516ca8cd7d5f707251ec7c4
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-30 09:50:48 +0200

    Move cart logic into custom hook

--- src/App.js
+++ src/App.js
@@ -1,108 +1,22 @@
 import './App.css';
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Homepage } from './components/Homepage'
 import { About } from './components/About'
 import { Shop } from './components/Shop'
 import { Cart } from './components/Cart'
-import { products } from './ProductLists'
+import { CartLogic } from './CartLogic'
 
 function App() {
-  const [cart, setCart] = useState([])
-
-  useEffect(() => {
-    console.log(cart)
-  }, [cart])
-
-  function handleCart(event) {
-    let selectedProduct = products.find((item) => item.id === +event.target.dataset.id)
-    switch (event.target.dataset.action) {
-      case 'add':
-        handlePlus(selectedProduct)
-        break;
-      case 'remove':
-        handleMinus(selectedProduct)
-        break;
-      default:
-        clearCart()
-    }    
-  }
-
-  function handlePlus(selectedProduct) {
-    if (checkPresence(selectedProduct) === false) {
-      addItem(selectedProduct)
-    } else {
-      incrementItem(selectedProduct)
-    }
-  }
-
-  function handleMinus(selectedProduct) {
-    if (checkPresence(selectedProduct) === false) return
-    if (findInCart(selectedProduct).quantity === 1) {
-      removeItem(selectedProduct)
-    } else {
-      decrementItem(selectedProduct)
-    }
-  }
-
-  function addItem(selectedProduct) {
-    let object = { product: selectedProduct,
-      quantity: 1
-    }
-    setCart(cart => [...cart, object])
-  }
-
-  function incrementItem(selectedProduct) {
-    let updatedCart = cart.map((item) => {
-      if (item.product === selectedProduct) {
-        item.quantity++
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function removeItem(selectedProduct) {
-    setCart(cart => cart.filter((item) => item.product !== selectedProduct))
-  }
-
-  function decrementItem(selectedProduct) {
-    let updatedCart = cart.map((item) => {
-      if (item.product === selectedProduct) {
-        item.quantity--
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function clearCart() {
-    setCart([])
-  }
-
-  function checkPresence(selectedProduct) {
-    if (cart.filter((item) => item.product === selectedProduct).length !== 0) return true
-    return false
-  }
-
-  function findInCart(selectedProduct) {
-    return cart.find((item) => item.product === selectedProduct)
-  }
-
-  function countItems() {
-    let count = cart.reduce((pV, cV) => pV + cV.quantity, 0)
-    if (count === 0) return null
-    return count
-  }
+  const useCart = CartLogic()
 
   return (
     <BrowserRouter>
-    <Navbar itemsCount={countItems()} />
+    <Navbar itemsCount={useCart.countItems()} />
       <Routes>
         <Route path="" element={<Homepage />}></Route>
         <Route path="about" element={<About />}></Route>
-        <Route path="shop" element={<Shop cart={cart} handleCart={handleCart} />}></Route>
+        <Route path="shop" element={<Shop cart={useCart.cart} handleCart={useCart.handleCart} />}></Route>
         <Route path="cart" element={<Cart />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/CartLogic.js
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react'
+import { products } from './ProductLists'
+
+const CartLogic = () => {
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        console.log(cart)
+    }, [cart])
+
+    function handleCart(event) {
+        let selectedProduct = products.find((item) => item.id === +event.target.dataset.id)
+        switch (event.target.dataset.action) {
+            case 'add':
+            handlePlus(selectedProduct)
+            break;
+            case 'remove':
+            handleMinus(selectedProduct)
+            break;
+            default:
+            clearCart()
+        }    
+    }
+
+    function handlePlus(selectedProduct) {
+        if (checkPresence(selectedProduct) === false) {
+            addItem(selectedProduct)
+        } else {
+            incrementItem(selectedProduct)
+        }
+    }
+
+    function handleMinus(selectedProduct) {
+        if (checkPresence(selectedProduct) === false) return
+        if (findInCart(selectedProduct).quantity === 1) {
+            removeItem(selectedProduct)
+        } else {
+            decrementItem(selectedProduct)
+        }
+    }
+
+    function addItem(selectedProduct) {
+        let object = { product: selectedProduct,
+            quantity: 1
+        }
+        setCart(cart => [...cart, object])
+    }
+
+    function incrementItem(selectedProduct) {
+        let updatedCart = cart.map((item) => {
+            if (item.product === selectedProduct) {
+            item.quantity++
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function removeItem(selectedProduct) {
+        setCart(cart => cart.filter((item) => item.product !== selectedProduct))
+    }
+
+    function decrementItem(selectedProduct) {
+    let updatedCart = cart.map((item) => {
+        if (item.product === selectedProduct) {
+        item.quantity--
+        }
+        return item
+    })
+        setCart(updatedCart)
+    }
+
+    function clearCart() {
+        setCart([])
+    }
+
+    function checkPresence(selectedProduct) {
+        if (cart.filter((item) => item.product === selectedProduct).length !== 0) return true
+        return false
+    }
+
+    function findInCart(selectedProduct) {
+        return cart.find((item) => item.product === selectedProduct)
+    }
+
+    function countItems() {
+        let count = cart.reduce((pV, cV) => pV + cV.quantity, 0)
+        if (count === 0) return null
+        return count
+    }
+
+    return {
+        cart,
+        handleCart,
+        countItems
+    }
+}
+
+export { CartLogic }
